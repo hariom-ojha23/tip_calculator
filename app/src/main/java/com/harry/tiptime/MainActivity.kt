3c3753dd9f7a0c2b0e9d41ddca269b22bd43f2da
hariom-ojha23 <hari_om.scsebtech@galgotiasuniversity.edu.in>
2021-03-25 23:52:29 +0530

    enhancing user experiance and changing icon color

--- app/src/main/java/com/harry/tiptime/MainActivity.kt
+++ app/src/main/java/com/harry/tiptime/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.harry.tiptime
 
+import android.content.Context
 import android.icu.text.NumberFormat
 import android.os.Build
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.harry.tiptime.databinding.ActivityMainBinding
@@ -17,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateBtn.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
@@ -43,5 +48,16 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
